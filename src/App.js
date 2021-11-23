feb6cffea83a2c76bc77d7fe52d1f0081b74a4b6
Gustavo <gustavo.prog40@gmail.com>
2021-11-23 08:56:12 -0300

    ind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import AudioPlayer from 'material-ui-audio-player'
+import Mirror from './assets/Mirror.wav'
+import Cover from './assets/Cover.wav'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ color: 'red' }}> Red Pill</h1>
+        <h4>Mirror</h4>
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={Mirror}
+        />
+        <br />
+        <h4>Cover - Aghatocles</h4>
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={Cover}
+        />
+        <br />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
